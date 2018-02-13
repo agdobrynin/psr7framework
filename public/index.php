fd4065b0c2cc8abb558a4b6d632fa2d18816b3ed
Alexey Dobrynin <ag.dobrynin@gmail.com>
2018-02-13 22:07:30 +0400

    Add response sender

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\ServerRequestFactory;
+use Framework\Http\ResponseSender;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -11,8 +12,5 @@ $name = $requet->getQueryParams()['name'] ?? 'Guest';
 $response = (new HtmlResponse("Hello {$name}"))
     ->withHeader('X-Engine', 'Simple php framework');
 
-header('HTTP/1.0 '.$response->getStatusCode().' '.$response->getReasonPhrase());
-foreach ($response->getHeaders() as $name=>$vals) {
-    header($name . ':' . implode(', ', $vals));
-}
-echo $response->getBody();
+$emitter = new ResponseSender();
+$emitter->send($response);
