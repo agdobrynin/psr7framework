27c2ec6a210c900a07611459345ed2588dbed26c
Alexey Dobrynin <ag.dobrynin@gmail.com>
2018-02-19 23:56:57 +0400

    Add comments

--- public/index.php
+++ public/index.php
@@ -4,13 +4,15 @@ use Zend\Diactoros\ServerRequestFactory;
 use Zend\Diactoros\Response\SapiEmitter;
 
 require __DIR__ . '/../vendor/autoload.php';
-
 $requet = ServerRequestFactory::fromGlobals();
 
+// почти контроллер :)
 $name = $requet->getQueryParams()['name'] ?? 'Guest';
 
+// создаем новый HTML response
 $response = (new HtmlResponse("Hello {$name}"))
     ->withHeader('X-Engine', 'Simple php framework');
 
 $emitter = new SapiEmitter();
+// отправить ответ
 $emitter->emit($response);
