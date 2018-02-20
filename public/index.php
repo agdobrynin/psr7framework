934895beb68927e67dad52f7dad67606f109083e
Alexey Dobrynin <ag.dobrynin@gmail.com>
2018-02-20 21:41:56 +0400

    Add action as anonimous function

--- public/index.php
+++ public/index.php
@@ -1,34 +1,48 @@
 <?php
+use Psr\Http\Message\ServerRequestInterface;
+
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\ServerRequestFactory;
 use Zend\Diactoros\Response\SapiEmitter;
 
 require __DIR__ . '/../vendor/autoload.php';
-$requet = ServerRequestFactory::fromGlobals();
+$request = ServerRequestFactory::fromGlobals();
 
 // почти контроллер :)
-$path = $requet->getUri()->getPath();
+$path = $request->getUri()->getPath();
+$action = null;
 if ($path === '/') {
-    $name = $requet->getQueryParams()['name'] ?? 'Guest';
-    $response = new HtmlResponse("Hello {$name}");
+    $action = function (ServerRequestInterface $request) {
+        $name = $request->getQueryParams()['name'] ?? 'Guest';
+        return new HtmlResponse("Hello {$name}");
+    };
 } elseif ($path === '/about') {
-    $response = new HtmlResponse("About. Simple site.");
+    $action = function (ServerRequestInterface $request) {
+        return new HtmlResponse("About. Simple site.");
+    };
 } elseif ($path === '/blog') {
-    $response = new JsonResponse([
-        ['id'=>1, 'title'=>'First post'],
-        ['id'=>2, 'title'=>'Second post'],
-    ]);
-} elseif (preg_match('#^/blog/(?P<id>[0-9]+)$#i', $path, $matches)) {
-    if ($matches['id']) {
-        $response = new JsonResponse([
-            'id'=>$matches['id'],'title'=>"Post #{$matches['id']}"
+    $action = function (ServerRequestInterface $request) {
+        return new JsonResponse([
+            ['id'=>1, 'title'=>'First post'],
+            ['id'=>2, 'title'=>'Second post'],
         ]);
-    } else {
-        $response = new JsonResponse(['error'=>'Undefined post'], 404);
-    }
+    };
+} elseif (preg_match('#^/blog/(?P<id>[0-9]+)$#i', $path, $matches)) {
+    $request = $request->withAttribute('id', $matches['id']);
+    $action = function (ServerRequestInterface $request) {
+        $id = $request->getAttribute('id');
+        if ($id) {
+            return new JsonResponse(['id'=>$id,'title'=>"Post #{$id}"]);
+        }
+        return new JsonResponse(['error'=>'Undefined post'], 404);
+    };
+}
+
+if ($action) {
+    $response = $action($request);
 } else {
-    $response = new JsonResponse(['error'=>'Undefined page'], 404);
+    $response = new JsonResponse(['error'=>'Page not found'], 404);
 }
 
 // Post processing
