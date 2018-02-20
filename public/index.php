669183c38daa04027bf2a4d2da63b6b6651400bc
Alexey Dobrynin <ag.dobrynin@gmail.com>
2018-02-20 21:23:58 +0400

    Add route for posts

--- public/index.php
+++ public/index.php
@@ -9,12 +9,25 @@ $requet = ServerRequestFactory::fromGlobals();
 
 // почти контроллер :)
 $path = $requet->getUri()->getPath();
-if($path === '/') {
+if ($path === '/') {
     $name = $requet->getQueryParams()['name'] ?? 'Guest';
     $response = new HtmlResponse("Hello {$name}");
 } elseif ($path === '/about') {
     $response = new HtmlResponse("About. Simple site.");
-}else{
+} elseif ($path === '/blog') {
+    $response = new JsonResponse([
+        ['id'=>1, 'title'=>'First post'],
+        ['id'=>2, 'title'=>'Second post'],
+    ]);
+} elseif (preg_match('#^/blog/(?P<id>[0-9]+)$#i', $path, $matches)) {
+    if ($matches['id']) {
+        $response = new JsonResponse([
+            'id'=>$matches['id'],'title'=>"Post #{$matches['id']}"
+        ]);
+    } else {
+        $response = new JsonResponse(['error'=>'Undefined post'], 404);
+    }
+} else {
     $response = new JsonResponse(['error'=>'Undefined page'], 404);
 }
 
