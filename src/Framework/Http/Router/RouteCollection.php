dcdae5fabff41e2711191b5723e31724e76ffd7f
Alexey Dobrynin <ag.dobrynin@gmail.com>
2018-02-25 22:41:19 +0400

    Add new method into RouteCollection

--- src/Framework/Http/Router/RouteCollection.php
+++ src/Framework/Http/Router/RouteCollection.php
@@ -7,19 +7,24 @@ class RouteCollection
 {
     private $routes = [];
 
+    public function addRoute(Route $route): void
+    {
+        $this->routes[]=$route;
+    }
+
     public function get($name, $pattern, $handler, array $tokens =[]): void
     {
-        $this->routes[] = new Route($name, $pattern, $handler, ['GET'], $tokens);
+        $this->addRoute( new Route($name, $pattern, $handler, ['GET'], $tokens));
     }
 
     public function post($name, $pattern, $handler, array $tokens =[]): void
     {
-        $this->routes[] = new Route($name, $pattern, $handler, ['POST'], $tokens);
+        $this->addRoute(new Route($name, $pattern, $handler, ['POST'], $tokens));
     }
 
     public function any($name, $pattern, $handler, array $tokens =[]): void
     {
-        $this->routes[] = new Route($name, $pattern, $handler, [], $tokens);
+        $this->addRoute(new Route($name, $pattern, $handler, [], $tokens));
     }
 
     public function getRoutes(): array
