4a063bb888e7c17c2c26ae379271f962b993adf8
Alexey Dobrynin <ag.dobrynin@gmail.com>
2018-03-10 17:06:29 +0400

    Lesson 3, time 1:25:00

--- src/App/Http/Middleware/NotFoundHandler.php
+++ src/App/Http/Middleware/NotFoundHandler.php
@@ -7,6 +7,13 @@ use Zend\Diactoros\Response\HtmlResponse;
 
 class NotFoundHandler
 {
+    /**
+     * Undocumented function
+     *
+     * @param ServerRequestInterface $request 
+     * 
+     * @return Zend\Diactoros\Response\HtmlResponse
+     */
     public function __invoke(ServerRequestInterface $request)
     {
         return new HtmlResponse('Undefined page :-(', 404);
