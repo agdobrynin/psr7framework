9df144e63ab311a5a6aed2fd3e3c5bc027632506
Alexey Dobrynin <ag.dobrynin@gmail.com>
2018-03-11 17:40:44 +0400

    Add method run in Application class

--- src/Framework/Http/Appilcation.php
+++ src/Framework/Http/Appilcation.php
@@ -3,20 +3,35 @@ namespace Framework\Http;
 
 use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Pipeline\Pipeline;
+use Psr\Http\Message\ServerRequestInterface;
 
 class Appilcation extends Pipeline
 {
+    /**
+     * Резоллвер для миддлвар
+     *
+     * @var Framework\Http\Pipeline\MiddlewareResolver
+     */
     private $_resovler;
+
+    /**
+     * Действие если нет разрешенных действий (мидлвар)
+     *
+     * @var callable
+     */
+    private $_default;
 
     /**
      * Undocumented function
      *
-     * @param MiddlewareResolver $resolver 
+     * @param MiddlewareResolver $resolver  
+     * @param callable           $default  
      */
-    public function __construct(MiddlewareResolver $resolver)
+    public function __construct(MiddlewareResolver $resolver, callable $default)
     {
         parent::__construct();
         $this->_resovler = $resolver;
+        $this->_default = $default;
     }
 
     /**
@@ -29,5 +44,17 @@ class Appilcation extends Pipeline
     public function pipe($middleware): void
     {
         parent::pipe($this->_resovler->resolve($middleware));
+    }
+
+    /**
+     * Зупск экземпляра приложения
+     *
+     * @param ServerRequestInterface $request 
+     * 
+     * @return mixed
+     */
+    public function run(ServerRequestInterface $request)
+    {
+        return $this($request, $this->_default);
     }
 }
